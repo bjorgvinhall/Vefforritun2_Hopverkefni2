5df6e2c86b2f6f03f5ccd80a8d98ab2abca039f6
bjorgvinhall <43784017+bjorgvinhall@users.noreply.github.com>
2019-04-13 15:18:08 +0000

    apiurl og smá grunnur

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -10,7 +10,11 @@ export default function Home() {
         <h1 className="header__title">
           <Link className="header__titleLink" to="/">Vefforritunarbúðin</Link>
         </h1>
-        <NavLink activeClassName="header__link--selected" exact to="/cart">Karfa</NavLink>
+        <NavLink className="header__link" activeClassName="header__link--selected" exact to="/register">Nýskrá</NavLink>
+        <NavLink className="header__link" activeClassName="header__link--selected" exact to="/login">Innskrá</NavLink>
+        <NavLink className="header__link" activeClassName="header__link--selected" exact to="/cart">Karfa</NavLink>
+        <NavLink className="header__link" activeClassName="header__link--selected" exact to="/products">Nýjar vörur</NavLink>
+        <NavLink className="header__link" activeClassName="header__link--selected" exact to="/categories">Flokkar</NavLink>
       </div>
     </header>
   );
