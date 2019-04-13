c9567a1dfce81da9b1b2212d64de064c816ac4b3
emmalif96 <43357822+emmalif96@users.noreply.github.com>
2019-04-13 17:30:17 +0000

    header kominn

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,14 +7,22 @@ export default function Home() {
   return (
     <header className="header">
       <div className="header__content">
+      <div className="header__contentTitle">
         <h1 className="header__title">
           <Link className="header__titleLink" to="/">Vefforritunarbúðin</Link>
         </h1>
+        </div>
+        <div className="header__contentLink">
+        <div className="header__contentLinkUpper">
         <NavLink className="header__link" activeClassName="header__link--selected" exact to="/register">Nýskrá</NavLink>
         <NavLink className="header__link" activeClassName="header__link--selected" exact to="/login">Innskrá</NavLink>
         <NavLink className="header__link" activeClassName="header__link--selected" exact to="/cart">Karfa</NavLink>
-        <NavLink className="header__link" activeClassName="header__link--selected" exact to="/products">Nýjar vörur</NavLink>
+        </div>
+        <div className="header__contentLinkLower">
+        <NavLink className="header__link" activeClassName="header__link--selected" exact to="/">Nýjar vörur</NavLink>
         <NavLink className="header__link" activeClassName="header__link--selected" exact to="/categories">Flokkar</NavLink>
+        </div>
+      </div>
       </div>
     </header>
   );
