5b8d28ab4514eff93abd88d9b944b16f6ea2c0de
emmalif96 <43357822+emmalif96@users.noreply.github.com>
2019-04-13 22:44:12 +0000

    sma virkni i register

--- src/routes/register/Register.tsx
+++ src/routes/register/Register.tsx
@@ -1,38 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Register.scss';
+import Button from '../../components/button/Button';
+import Input from '../../components/input/Input'
 
-export default function Register() {
-  return (
-    <div className="register">
-      <h1 className="register__title">
-        Nýskráning
-      </h1>
-      <div className="register__form">
-      <div className="register__field">
-      <label className="register__label">
-      Notendanafn:
-      </label>
-      <input className="register__input" type="text"></input>
-      </div>
-      <div className="register__field">
-      <label className="register__label">
-      Lykiorð:
-      </label>
-      <input className="register__input" type="text"></input>
-      </div>
-      <div className="register__field">
-      <label className="register__label">
-      Email:
-      </label>
-      <input className="register__input" type="text"></input>
-      </div> 
-        <div className="register__button">
-        <button>Nýskrá</button>
-        </div>
-        <p>Innskráning</p>
-      </div>
+
+
+export default function Register(props:any) {
+  const [data, setData] = useState({ user: '', password: '', email: '' });
   
+  async function onSubmit() {
+    // e.preventDefault();
+    console.log(data.user);
+
+  }
+
+  function onChangeUser(e: any){
+    setData({
+      ...data,
+      user: e.target.value,
+    });
+    console.log("e.target", data.user)
+  }
+  
+  return (
+    <div className={'register__wrapper'}>
+    <h1>Nýskráning</h1>
+    <div className="register__form">
+    <Input
+      label={'Notendanafn:'}
+      onChange={onChangeUser}>
+    </Input>
+    <Input
+    label={'Lykilorð:'}>
+    </Input>
+    <Input
+    label={'Netfang:'}>
+    </Input>
     </div>
+
+    <div className="register__button">
+      <Button 
+      onClick={onSubmit}
+        >Nýskrá
+      </Button>
+    </div>
+  <p>Innskráning</p>
+</div>
+  
+   
   )
 }
