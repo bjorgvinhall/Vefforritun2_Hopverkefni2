5edd0b7f0e6c3ec31e259644f535e37cbdbaf69f
fah13 <fah13@hi.is>
2019-04-18 22:04:30 +0000

    Bara hægt að skoða orders ef maður er skráður inn

--- src/routes/orders/Orders.tsx
+++ src/routes/orders/Orders.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom'
+import Helmet from 'react-helmet';
 
 import './Orders.scss';
 
 export default function Orders() {
+  const username = localStorage.getItem('username');
+
+  if (!username) {
+    return (
+      <Fragment>
+        <Helmet title="Pantanir" />
+        <div className="cart">
+          <h2>Vinsamlegast skráðu þig inn til þess að skoða pantanir.</h2>
+          <Link to="/login" className="register__linkToLogin">Innskráning</Link>
+        </div>
+      </Fragment>
+    );
+  }
   return (
-    <p>orders</p>
+    <Fragment>
+      <Helmet title="Pantanir" />
+      <div className="orders">
+        <p>orders</p>
+      </div>
+    </Fragment>
   );
 }
