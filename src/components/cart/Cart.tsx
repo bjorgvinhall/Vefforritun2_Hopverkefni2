bbbebdfcc2b78664fe3984e4739a822f2071ad09
fah13 <fah13@hi.is>
2019-04-19 01:22:58 +0000

    Uppfæra

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,14 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Cart.scss';
 import {Link} from 'react-router-dom'
 
+import { updateCart, removeFromCart } from '../../api/index';
 import Button from '../../components/button/Button';
+import { IProduct } from '../../api/types';
+import { async } from 'q';
 
 export default function Cart(props: any) {
   const { product, onClick } = props;
+  
+  const [details, setDetails] = useState({} as IProduct);
+  const [cartLoading, setCartLoading] = useState(false);
+  const [cartMessage, setCartMessage] = useState('');
+  const [quantity, setQuantity] = useState(0);
+
   function klikk(e: any, id: number){
     if(onClick) onClick(id);
   }
+
+  function onChangeQuantity(e: any) {
+    setQuantity(e.target.value)
+  }
+
+  async function updateCartHandler() {
+    setCartLoading(true);
+    setCartMessage('');
+    if(quantity < 1) {
+      setCartMessage('Vinsamlegast veljið fjölda stærri en 0');
+      setCartLoading(false)
+      return;
+    }
+    const result = await updateCart(details.id, quantity);
+    
+    if(result.success){
+      setCartMessage('Karfa uppfærð!');
+    } else {
+      setCartMessage('Villa kom upp')
+    }
+
+    setCartLoading(false);
+    setQuantity(0)
+  }
+
+  async function removeFromCartHandler() {
+    setCartMessage('');
+    const result = await removeFromCart(product.id);
+    if(result.success){
+      setCartMessage('Karfa uppfærð!');
+    } else {
+      setCartMessage('Villa kom upp')
+    }
+
+    setCartLoading(false);
+  }
+
   return (
     <Link to={`/cart`}  className="cart__item" style={{ textDecoration: 'none', color: '#000' }}>
       
@@ -25,18 +71,23 @@ export default function Cart(props: any) {
       <div className="cart__form">
         <div className="cart__form__top">
           <span>Fjöldi:</span>
-          <input className="cart__form__input" type="textarea" ></input>
+          <input className="cart__form__input" onChange={onChangeQuantity} type="number" min="1" value={product.quantity}></input>
           <Button
+            onClick={updateCartHandler}
             small={true}
             children="Uppfæra"
             // TODO onclick handler til að bæta í körfu + loading state
           ></Button>
+          {cartMessage && (
+            <span className="cart__form__top__cartMessage">{cartMessage}</span>
+          )}
         </div>
         <div className="cart__form__middle">
           <p>Samtals: {product.price} kr.-</p>
         </div>
         <div className="cart__form__bottom">
           <Button
+            onClick={removeFromCartHandler}
             small={true}
             children="Eyða línu"
             // TODO onclick handler til að bæta í körfu + loading state
